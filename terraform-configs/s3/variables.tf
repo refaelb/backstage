db7fb389c445548e42f07afa6e842b335b76a148
refael ben-chamo <refael@infini-t.ai>
2024-07-05 02:07:23 +0300

    test

--- terraform-configs/s3/variables.tf
+++ terraform-configs/s3/variables.tf
@@ -1,25 +1,4 @@
-variable "REGION" {
-    description = "default region"
-  
-}
-
 variable "PROFILE" {
     description = "profile name"
-  
-}
-
-variable "BUCKET" {
-  description = "Bucket Name for ALB State"
-}
-
-variable "DOMAIN_NAME" {
-  description = "Domain Name"
-}
-variable "ACM_DOMAIN_NAME" {
-  description = "Acm Domain Name"
-}
-
-variable "ACM_ARN" {
-  description = "Acm arn"
 }
 
--- terraform-tfvars/s3/s3.tfvars
+++ terraform-tfvars/s3/s3.tfvars
@@ -1,2 +1,1 @@
 PROFILE = "test"
-BUCKET = "test"
